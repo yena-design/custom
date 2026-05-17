8573c1c50b683f4c8c759338d93064aac847bc81
yena-design <matt@recordpathai.com>
2026-05-17 00:45:45 -0700

    Reposition site around free eligibility and paid court packet generation

---
+++ js/court-filing-methods.js
@@ -0,0 +1,14 @@
+const courtFilingMethods = {
+  "ohio-wood-municipal": {
+    filingMethod: "mail-or-in-person",
+    acceptsOnline: false,
+    acceptsEmail: false,
+    requiresNotary: true,
+    requiresFingerprint: true,
+    filingFeeRequired: true,
+    portalUrl: "",
+    clerkEmail: "",
+    courtAddress: "711 S Dunbridge Rd, Bowling Green, OH 43402",
+    notes: "User should confirm current requirements with the clerk before filing."
+  }
+};
